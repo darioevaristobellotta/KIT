7b5bb3d21f640d52441d36e608320c4bee1dab7b
darioevaristobellotta <faith.qq.de@gmail.com>
2026-07-24 10:35:47 +0200

    feat(seo): explicitly allow all AI crawlers and search bots in robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,31 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [],
+      },
+      {
+        userAgent: [
+          'GPTBot',
+          'ChatGPT-User',
+          'ClaudeBot',
+          'Claude-Web',
+          'PerplexityBot',
+          'Google-Extended',
+          'Applebot-Extended',
+          'AnthropicAI',
+          'CCBot',
+          'cohere-ai',
+          'Omgilibot',
+          'FacebookBot',
+          'ByteSpider'
+        ],
+        allow: '/',
+        disallow: [],
+      }
+    ],
     sitemap: 'https://www.kit.darioevaristobellotta.de/sitemap.xml',
   };
 }
